cd48f961fb2234b40eb5b2882c508e05808daed8
Ajey <ajey.091@gmail.com>
2018-08-05 13:20:15 -0700

    Updated

--- RotateMatrix.py
+++ RotateMatrix.py
@@ -1,1 +1,16 @@
 def RotateMatrix(matrix1):
+    newmat = [[0]*len(matrix1) for i in range(len(matrix1))]
+    linenum = len(matrix1) -1
+    for i in range(0,len(newmat)):
+        for j in range(0,len(newmat)):
+            newmat[j][linenum] = matrix1[i][j]
+
+        linenum -= 1
+
+    return newmat
+
+print (RotateMatrix([[1,2,3,4],
+[4,5,6,5],
+[7,8,9,6],
+[7, 8, 9, 10]]
+))
